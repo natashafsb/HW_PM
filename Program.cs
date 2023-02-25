bcdf3e8254e1c46570f6004a043e8abfeb0981f8
natashafsb <skvernenko@mail.ru>
2023-02-25 20:39:45 +0300

    make a programm

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Elements massive's number = ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    string[] a = new string[n];
+    string[] b = new string[n];
+    for(int i = 0; i < n; i++)
+    {
+        a[i] = Console.ReadLine();
+    }
+    int t = 0;
+    for(int i = 0; i < n; i++){
+        int length = a[i].Length;
+        if (length <= 3){
+            b[t] = a[i];
+            t = t + 1;
+        }
+    }
+Console.WriteLine("New massive :");
+    for(int i = 0; i < n; i++)
+    {
+        Console.Write(b[i]);
+        Console.Write(' ');
+    }
